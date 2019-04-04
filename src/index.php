4e6c4d68ce04f8a61de07262394b19c220d8c6ef
devasyadave <38454922+devasyadave@users.noreply.github.com>
2019-04-04 12:06:03 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,8 +5,10 @@
  * Author: miniOrange
  */
 
-if(!isset($_SESSION))
-session_start();
+if(!isset($_SESSION)) {
+    session_id('connector');
+    session_start();
+}
 if (session_id() == 'connector' || session_id() == 'attributes') {
     if (is_user_registered()==NULL) {
         header("Location: register.php"); // https://www.google.com
